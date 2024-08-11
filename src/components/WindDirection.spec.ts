c49d19038941278f2ad9cec501731cc734f107d6
admin <sivenskij@gmail.com>
2024-08-11 22:17:27 +0300

    Добавлены тесты для компонента WindDirection
    1. Проверка отрисовки компонента
    2. Проверка корректной отрисовки стрелочки визуализирующей напривление ветра

---
+++ src/components/WindDirection.spec.ts
@@ -0,0 +1,23 @@
+import { shallowMount } from '@vue/test-utils';
+import WindDirection from './WindDirection.vue';
+
+describe('WindDirection', () => {
+	it('Simple renders without crashing', () => {
+		const wrapper = shallowMount(WindDirection, {
+			props : { degrees : 90 }
+		});
+
+		expect(wrapper).toBeTruthy();
+	});
+	it('Renders the indicator with the correct wind direction', () => {
+		const wrapper = shallowMount(WindDirection, {
+			props : { degrees : 90 }
+		});
+		const windDirectionArrow = wrapper.find('[data-testid="windDirectionArrow"');
+		const windDirectionArrowAttributeStyle = windDirectionArrow.attributes('style');
+		const windDirectionArrowInnerHTML = windDirectionArrow.html();
+
+		expect(windDirectionArrowAttributeStyle).toContain('rotate(90deg)');
+		expect(windDirectionArrowInnerHTML).toContain('⬇');
+	});
+});
